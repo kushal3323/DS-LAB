413a6d70715eebc66eca419e2eac4cdb931b0dab
kushal3323 <kushalmurarka.cs24@bmsce.ac.in>
2025-12-25 00:30:55 +0530

    Update lab1.c

--- lab1.c
+++ lab1.c
@@ -1,74 +1,83 @@
 #include<stdio.h>
 #define n 5
+
 int stack[5];
-int top=-1;
+int top = -1;
 
-void push(){
-  int x;
-  printf("Enter the no:");
-  scanf("%d",&x);
-  if(top==n-1){
-    printf("overflow");
-  }
-  else{
-      ++top;
-    stack[top]=x;
-    
-    printf("%d",stack[top]);
-  }
-}
-void pop(){
-    int item;
-    if(top==-1){
-        printf("Stack is empty,underflow");
-    }
-    else{
-        item=stack[top];
-        top--;
-        printf("%d",item);
-
+void push() {
+    int x;
+    if (top == n - 1) {
+        printf("Stack overflow\n");
+    } else {
+        printf("Enter the number: ");
+        scanf("%d", &x);
+        stack[++top] = x;
+        printf("Element pushed successfully\n");
     }
 }
-int peek(){
-    if(top==-1){
-        printf("Stack is empty");
 
+void pop() {
+    if (top == -1) {
+        printf("Stack underflow\n");
+    } else {
+        printf("Popped element: %d\n", stack[top--]);
     }
-    else{
+}
+
+int peek() {
+    if (top == -1) {
+        printf("Stack is empty\n");
+        return -1;
+    } else {
         return stack[top];
     }
 }
- void display(){
-   if(top==-1){
-    printf("empty stack,underflow");
-   }
-   else{
-    for(int i=top;i>=0;i--){
-        printf("%d",stack[i]);
-    }
-   }
- }
- int main(){
-    int choice;
-        while(1){
-        printf("1.push\n2.pop\n3.peek\n4.display\n5.exit\n");
-        printf("Enter your choice:");
-        scanf("%d",&choice);
-        switch(choice){
-            case 1: push();
-                    break;
-            case 2: pop();
-                    break;
-            case 3: peek();
-                    break;
-            case 4: display();
-                    break;
-            
-            default: printf("Invalid choice");
-        }
-        }
- 
 
+void display() {
+    if (top == -1) {
+        printf("Stack is empty\n");
+    } else {
+        printf("Stack elements are:\n");
+        for (int i = top; i >= 0; i--) {
+            printf("%d\n", stack[i]);
+        }
+    }
 }
 
+int main() {
+    int choice;
 
+    while (1) {
+        printf("\n1.push\n2.pop\n3.peek\n4.display\n5.exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                push();
+                break;
+
+            case 2:
+                pop();
+                break;
+
+            case 3: {
+                int val = peek();
+                if (val != -1)
+                    printf("Top element: %d\n", val);
+                break;
+            }
+
+            case 4:
+                display();
+                break;
+
+            case 5:
+                printf("Exiting program\n");
+                return 0;
+
+            default:
+                printf("Invalid choice\n");
+        }
+    }
+}
